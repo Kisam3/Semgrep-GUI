523fe7b9aac65378ae1aaa2ecd63617652f1f470
mjsin <microrider1997@gmail.com>
2023-11-11 11:32:37 +0530

    v0.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import FileUpload from './upload';
 import ResultsPage from "./fetch";
 import {BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import Landing from "./landingpage";
->>>>>>> UI-Development
 
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-<<<<<<< HEAD
-=======
                 <Route path = "/" element = {<Landing />} />
->>>>>>> UI-Development
                 <Route path = "/upload" element = {<FileUpload />} />
                 <Route path = "/result" element = {<ResultsPage />} />
             </Routes>
